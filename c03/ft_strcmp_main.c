f1fa29e1ccaa40744a9578b3f865821678ae019f
SurSargsyan <137448149+SurSargsyan@users.noreply.github.com>
2024-12-06 16:07:42 +0100

    Create ft_strcmp_main.c

---
+++ c03/ft_strcmp_main.c
@@ -0,0 +1,7 @@
+int	main()
+{
+	printf("%d", ft_strcmp("Hello", "Hello1"));
+	printf("\n%d", ft_strcmp("Hello", "He"));
+	printf("\n%d", ft_strcmp("He", "Hello"));
+	printf("\n%d", ft_strcmp("Hello", "Hello"));
+}
