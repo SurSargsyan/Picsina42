be9eed675ee87523030bae2ab9cb5f810ccb5035
SurSargsyan <137448149+SurSargsyan@users.noreply.github.com>
2024-12-06 15:55:33 +0100

    Create ft_str_is_uppercase_main.c

---
+++ c02/ex05/ft_str_is_uppercase_main.c
@@ -0,0 +1,6 @@
+int	main()
+{
+       printf("%d", ft_str_is_uppercase("ABCDEFGH"));
+       printf("\n%d", ft_str_is_uppercase("ABCDEFGHaI"));
+       printf("\n%d", ft_str_is_uppercase("-_134556ABCDEFGaH67"));
+}
