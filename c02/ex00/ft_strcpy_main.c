ad26e070c23342525214ea15ba55a0aa1e5190a7
SurSargsyan <137448149+SurSargsyan@users.noreply.github.com>
2024-12-06 15:44:55 +0100

    Create ft_strcpy_main.c

---
+++ c02/ex00/ft_strcpy_main.c
@@ -0,0 +1,9 @@
+int	main()
+{
+	char src[] = "beautiful";
+	char src1[] = "beautiful";
+	char dest[] = "Wo";
+	char dest1[] = "Wo";
+	printf("%s", ft_strcpy(dest, src));
+	printf("\n%s", strcpy(dest1, src1));
+}
