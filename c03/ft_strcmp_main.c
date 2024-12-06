78bab7aa78c3f0c2f697608e9a1a1db03e39e94b
SurSargsyan <137448149+SurSargsyan@users.noreply.github.com>
2024-12-06 16:08:57 +0100

    Delete c03/ex00/ft_strcmp_main.c

--- c03/ft_strcmp_main.c
+++
@@ -1,7 +0,0 @@
-int	main()
-{
-	printf("%d", ft_strcmp("Hello", "Hello1"));
-	printf("\n%d", ft_strcmp("Hello", "He"));
-	printf("\n%d", ft_strcmp("He", "Hello"));
-	printf("\n%d", ft_strcmp("Hello", "Hello"));
-}
