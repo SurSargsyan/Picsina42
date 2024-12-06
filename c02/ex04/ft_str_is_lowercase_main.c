925f7a1cc5e5322d8dc8a2104d99ab3add362ed4
SurSargsyan <137448149+SurSargsyan@users.noreply.github.com>
2024-12-06 15:54:09 +0100

    Create ft_str_is_lowercase_main.c

---
+++ c02/ex04/ft_str_is_lowercase_main.c
@@ -0,0 +1,6 @@
+int	main()
+{
+	printf("%d", ft_str_is_lowercase("abcdefghijkl"));
+        printf("\n%d", ft_str_is_lowercase("abcAdefghijkl"));
+        printf("\n%d", ft_str_is_lowercase("-_134556efSghij67"));
+}
